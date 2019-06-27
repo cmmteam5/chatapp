aa59b27558f364d463b6895ca4b3d7bf26d8fc14
Yin Nyein Aye <yinnyeinaye.yinnyein@gmail.com>
2019-06-27 10:14:38 +0630

    to pull request routing has changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,12 @@
 Rails.application.routes.draw do
-  #get 'group/index'
-  #get 'group/new'
- # get 'group/edit'
-  #get 'group/show'
-  #get 'workspaces/index'
-  #get 'workspaces/new'
-  #get 'workspaces/update'
- #get 'workspaces/delete'
- # get 'workspaces/show'
-  root 'static_pages#home'
-  #root 'workspaces#index'
-  get '/new', to: 'workspaces#new'
-  get '/login', to: 'static_pages#login'
-  get '/home', to: 'static_pages#home'
-  get '/menu', to: 'group#menu'
- # get '/index', to: 'group#index'
-  get '/home', to: 'static_pages#home' 
-  get '/signup', to: 'static_pages#signup'
-  get '/index', to: 'workspaces#index'
-  #get 'workspaces/delete'
-  #get 'workspaces/show'
-  #root 'static_pages#home'
-  #root 'workspaces#index'
+
+ 
   resources :workspaces
- # root 'group#index'
+
 
   
 
 
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
