c499680b5e262f6221df370f4fedc3ca1fcb0baf
MayThuMyat <maythumyat@mm.cybermissions.co.jp>
2019-07-30 09:15:19 +0630

    localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     include SessionHelper
+    before_action :set_locale
+     private
+     def set_locale
+       I18n.locale = params[:locale] || session[:locale]|| I18n.default_locale
+       session[:locale]=I18n.locale
+     end
+    
+ 
+    private
+     def default_url_options
+           { locale: I18n.locale }
+     end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,8 @@ class User < ApplicationRecord
 	has_many :workspaces, through: :userworkspaces
 	has_many :groupusers, dependent: :destroy
 	has_many :groups, through: :groupusers
-<<<<<<< HEAD
-	has_many :groupconversations, dependent: :destroy  
-	validates :name,length: { minimum: 6 }
-	validates :email,length: { minimum: 6 }
-=======
 	has_many :groupconversations, dependent: :destroy 
 	validates :name, presence: true, length: { minimum: 6 }
 	validates :email, presence: true, length: { minimum: 6 }
->>>>>>> 76932a19b7c98bbff72ab5abc26d355a0e8af2bd
 	has_secure_password
 end
